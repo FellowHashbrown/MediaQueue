0d8d2b25ab6eaca6bab0aa40a3a100338a3d0817
Jonah Pierce <fellowhashbrown@gmail.com>
2021-02-25 16:44:24 -0500

    Added movie object

---
+++ media/movie.py
@@ -0,0 +1,86 @@
+import os
+from json import dump, load
+from typing import Union
+
+from media import Media, StreamingProvider, Person
+
+
+class Movie(Media):
+    """A Movie object is a single type of media
+
+    :param name: The name of the Movie
+    :param runtime: The runtime of this Movie in minutes
+    :param provider: The Streaming Provider where this Movie is located on
+    :param person: The Person that is watching this Movie
+
+    :keyword started: Whether or not this Movie has been started (Defaults to False)
+    :keyword finished: Whether or not this Movie has been finished (Defaults to False)
+    :keyword json: The JSON object to load a Movie object from
+    :keyword filename: The JSON file to load a Movie object from
+
+    :raises FileNotFoundError: When the JSON file specified does not exist
+    :raises KeyError: When any required parameters are missing
+    :raises ValueError: When any parameters do not have a valid value
+    """
+
+    FOLDER = "movies"
+
+    def __init__(self, name: str = None, runtime: int = None,
+                 provider: Union[StreamingProvider, str] = None,
+                 person: Union[Person, str] = None,
+                 *, started: bool = False, finished: bool = False,
+                 json: dict = None, filename: str = None):
+        super().__init__(name, provider, person,
+                         started=started, finished=finished,
+                         json=json, filename=filename)
+
+        # Check if a JSON file was given
+        if filename is not None:
+            with open(filename, "r") as jsonfile:
+                json = load(jsonfile)
+
+        # Check if a JSON object was given
+        if json is not None:
+            if "runtime" not in json:
+                raise KeyError("Runtime must be specified")
+
+            runtime = json["runtime"]
+
+        # Validate the parameters values
+        if runtime is None:
+            raise ValueError("Runtime must be specified")
+
+        if runtime is not None and runtime <= 0:
+            raise ValueError("Runtime must be > 0")
+
+        self.__runtime = runtime
+
+    # # # # # # # # # # # # # # # # # # # # # # # # #
+
+    def get_runtime(self, in_hours: bool = False) -> int:
+        """Returns the runtime of this Movie in minutes or hours
+
+        :param in_hours: Whether or not to get the runtime in hours
+        """
+        if in_hours:
+            return round(self.get_runtime() // 60)
+        return self.__runtime
+
+    # # # # # # # # # # # # # # # # # # # # # # # # #
+
+    def to_json(self) -> dict:
+        """Returns the JSON representation of this Movie"""
+        return {
+            "id": self.get_id(),
+            "name": self.get_name(),
+            "runtime": self.get_runtime(),
+            "provider": self.get_provider().value,
+            "person": self.get_person().value
+        }
+
+    def save(self):
+        """Saves this Movie into a JSON file"""
+        if not os.path.exists(Movie.FOLDER):
+            os.mkdir(Movie.FOLDER)
+        with open("./{}/{}".format(Movie.FOLDER, self.get_id()), "w") as jsonfile:
+            dump(self.to_json(), jsonfile, indent=4)
