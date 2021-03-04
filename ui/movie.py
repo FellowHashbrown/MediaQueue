7e09219c68632dd488870fab3299164d9697eea6
Jonah Pierce <fellowhashbrown@gmail.com>
2021-03-04 17:37:56 -0500

    Added documentation to MovieDialog and tool tips

--- ui/movie.py
+++ ui/movie.py
@@ -2,120 +2,195 @@ import sys
 from functools import partial
 from PyQt5 import QtWidgets, QtCore
 
-from media import Movie
-from ui import MessageBox, add_grid_to_layout
+from media import Movie, StreamingProvider, Person
+from ui import MessageBox, add_grid_to_layout, media_objects
 
 
-class MovieView(QtWidgets.QWidget):
+class MovieDialog(QtWidgets.QDialog):
     def __init__(self, parent: QtWidgets.QWidget = None, flags=QtCore.Qt.WindowFlags()):
         super().__init__(parent, flags)
-        self.movie = None
-        self.callback = None
         self.index = None
 
-        self.setSizePolicy(QtWidgets.QSizePolicy.Preferred,
-                           QtWidgets.QSizePolicy.MinimumExpanding)
+        self.button_box_add = QtWidgets.QDialogButtonBox(
+            QtWidgets.QDialogButtonBox.Cancel | QtWidgets.QDialogButtonBox.Ok)
+        self.button_box_edit = QtWidgets.QDialogButtonBox(
+            QtWidgets.QDialogButtonBox.Cancel | QtWidgets.QDialogButtonBox.Save)
+
+        self.button_box_add.accepted.connect(partial(self.add, False))
+        self.button_box_add.rejected.connect(self.reject)
+        self.button_box_edit.accepted.connect(partial(self.add, True))
+        self.button_box_edit.rejected.connect(self.reject)
+
+        self.name_line_edit = None
+        self.hours_spinner = None
+        self.minutes_spinner = None
+        self.provider_dropdown = None
+        self.person_dropdown = None
+        self.start_checkbox = None
+        self.finish_checkbox = None
+
+        self.setup_ui()
+        self.result = self.exec_()
+
+    # # # # # # # # # # # # # # # # # # # # # # # # #
+
+    def setup_ui(self):
+
         layout = QtWidgets.QGridLayout()
 
-        name_label = QtWidgets.QLabel("Name", self)
-        runtime_label = QtWidgets.QLabel("Runtime (in minutes)", self)
-        provider_label = QtWidgets.QLabel("Streaming Provider", self)
-        person_label = QtWidgets.QLabel("Person", self)
-
-        self.name_line_edit = QtWidgets.QLineEdit(self)
-        self.runtime_spinner = QtWidgets.QSpinBox(self)
-        self.runtime_spinner.setRange(0, 1000)
-        self.provider_dropdown = QtWidgets.QComboBox(self)
-        self.provider_dropdown.addItems(["Vudu", "Disney+", "Netflix", "Hulu", "HBO Max"])
-        self.person_dropdown = QtWidgets.QComboBox(self)
-        self.person_dropdown.addItems(["Jonah", "Taylor", "Both"])
-
-        self.cancel_button = QtWidgets.QPushButton("Cancel")
-        self.cancel_button.clicked.connect(self.cancel)
-        self.save_button = QtWidgets.QPushButton("Save")
-        self.save_button.clicked.connect(partial(self.add, True))
-        self.add_button = QtWidgets.QPushButton("Add")
-        self.add_button.clicked.connect(self.add)
-        self.start_checkbox = QtWidgets.QCheckBox("Started?", self)
-        self.finish_checkbox = QtWidgets.QCheckBox("Finished?", self)
-
-        grid = [[name_label, self.name_line_edit, None],
-                [runtime_label, self.runtime_spinner, None],
-                [provider_label, self.provider_dropdown, None],
-                [person_label, self.person_dropdown, None],
-                [self.start_checkbox, self.finish_checkbox],
-                [None, None]]
-
-        add_grid_to_layout(grid, layout)
-        layout.addWidget(self.cancel_button, 5, 0)
-        layout.addWidget(self.add_button, 5, 2)
-        layout.addWidget(self.save_button, 5, 2)
-        layout.setRowStretch(4, 1)
+        layout.addWidget(self.setup_movie_ui(self))
+        if media_objects.get_movie() is not None:
+            layout.addWidget(self.button_box_edit)
+            self.window().setWindowTitle(f"Edit {media_objects.get_movie().get_name()}")
+        else:
+            layout.addWidget(self.button_box_add)
+            self.window().setWindowTitle("Add Movie")
 
         self.setLayout(layout)
         self.show()
 
+    def setup_movie_ui(self, parent: QtWidgets.QWidget) -> QtWidgets.QWidget:
+        """Creates and returns the widgets for the Movie widgets
+        such as the Name Entry, Runtime spinner, Provider box, and Person box
+
+        :param parent: The parent widget of the Movie widgets
+        """
+
+        widget = QtWidgets.QWidget(parent)
+        layout = QtWidgets.QGridLayout()
+
+        name_label = QtWidgets.QLabel("Name", widget)
+        runtime_label = QtWidgets.QLabel("Runtime", widget)
+        hours_label = QtWidgets.QLabel("Hours", widget)
+        minutes_label = QtWidgets.QLabel("Minutes", widget)
+        provider_label = QtWidgets.QLabel("Streaming Provider", widget)
+        person_label = QtWidgets.QLabel("Person", widget)
+
+        self.name_line_edit = QtWidgets.QLineEdit(widget)
+        self.name_line_edit.setPlaceholderText("Movie Name")
+        self.name_line_edit.setToolTip("The name for the Movie")
+
+        self.hours_spinner = QtWidgets.QSpinBox(widget)
+        self.hours_spinner.setRange(0, 20)
+        self.hours_spinner.setToolTip("The hours portion of the runtime of the Movie")
+
+        self.minutes_spinner = QtWidgets.QSpinBox(widget)
+        self.minutes_spinner.setRange(0, 59)
+        self.minutes_spinner.setToolTip("The minutes portion of the runtime of the Movie")
+
+        self.provider_dropdown = QtWidgets.QComboBox(widget)
+        self.provider_dropdown.addItems([provider.value for provider in StreamingProvider])
+        self.provider_dropdown.setToolTip("The Streaming Provider for the Movie")
+
+        self.person_dropdown = QtWidgets.QComboBox(widget)
+        self.person_dropdown.addItems([person.value for person in Person])
+        self.person_dropdown.setToolTip("The Person watching the Movie")
+
+        self.start_checkbox = QtWidgets.QCheckBox("Started?", widget)
+        self.start_checkbox.clicked.connect(self.update_start)
+        self.start_checkbox.setToolTip("Mark the Movie as started")
+
+        self.finish_checkbox = QtWidgets.QCheckBox("Finished?", widget)
+        self.finish_checkbox.clicked.connect(self.update_finish)
+        self.finish_checkbox.setToolTip("Mark the Movie as finished")
+
+        movie = media_objects.get_movie()
+        if movie is not None:
+            hours, minutes = divmod(movie.get_runtime(), 60)
+            self.name_line_edit.setText(movie.get_name())
+            self.hours_spinner.setValue(hours)
+            self.minutes_spinner.setValue(minutes)
+            self.provider_dropdown.setCurrentText(movie.get_provider().value)
+            self.person_dropdown.setCurrentText(movie.get_person().value)
+            self.start_checkbox.setChecked(movie.is_started())
+            self.finish_checkbox.setChecked(movie.is_finished())
+
+        grid = [[name_label, self.name_line_edit, None, None, None],
+                [runtime_label, self.hours_spinner, hours_label, self.minutes_spinner, minutes_label],
+                [provider_label, self.provider_dropdown, None, None, None],
+                [person_label, self.person_dropdown, None, None, None],
+                [self.start_checkbox, self.finish_checkbox]]
+
+        add_grid_to_layout(grid, layout)
+        widget.setLayout(layout)
+        return widget
+
     # # # # # # # # # # # # # # # # # # # # # # # # #
 
-    def cancel(self):
-        """Resets the values for each widget and returns to the previous widget"""
+    def update_start(self):
+        """Updates the start and finish checkboxes for the Movie.
+
+        If a user checks the start checkbox, the finish checkbox will be unchecked
+        """
+
+        # Add 1 to the index when accessing the widgets
+        #   due to the column headings
+        started = self.start_checkbox.isChecked()
+        finished = self.finish_checkbox.isChecked()
+        if started and finished:
+            self.finish_checkbox.setChecked(False)
+        if media_objects.get_movie() is not None:
+            media_objects.get_movie().set_started(started)
+
+    def update_finish(self):
+        """Updates the start and finish checkboxes for the Movie.
+
+        If a user checks the finish checkbox, the start checkbox will be unchecked.
+        """
+
+        # Add 1 to the index when accessing the widgets
+        #   due to the column headings
+        finished = self.start_checkbox.isChecked()
+        started = self.finish_checkbox.isChecked()
+        if finished and started:
+            self.start_checkbox.setChecked(False)
+        if media_objects.get_movie() is not None:
+            media_objects.get_movie().set_finished(finished)
+
+    # # # # # # # # # # # # # # # # # # # # # # # # #
+
+    def clear_widgets(self):
+        """Cleats the widgets for the Movie and clears the attributes"""
+
         self.name_line_edit.setText("")
-        self.runtime_spinner.setValue(0)
+        self.hours_spinner.setValue(0)
+        self.minutes_spinner.setValue(0)
         self.provider_dropdown.setCurrentIndex(0)
         self.person_dropdown.setCurrentIndex(0)
-        self.callback()
+        self.start_checkbox.setChecked(False)
+        self.finish_checkbox.setChecked(False)
 
     def add(self, is_saving: bool = False):
-        """Saves the current Movie as an object and loads it into the
-        Media Queue app
+        """Creates the movie object from the widget values
+
+        If a value is invalid in the Movie object,
+        a Message Box will appear saying that something is wrong
         """
         try:
             if is_saving:
-                id = self.movie.get_id()
-            self.movie = Movie(
+                movie = media_objects.get_movie()
+                id = movie.get_id()
+            movie = Movie(
                 self.name_line_edit.text(),
-                self.runtime_spinner.value(),
+                self.hours_spinner.value() * 60 + self.minutes_spinner.value(),
                 self.provider_dropdown.currentText(),
                 self.person_dropdown.currentText(),
                 started=self.start_checkbox.isChecked(),
                 finished=self.finish_checkbox.isChecked()
             )
             if is_saving:
-                self.movie.set_id(id)
-            self.movie.save()
-            self.callback(self.index, self.movie)
+                movie.set_id(id)
+            movie.save()
+            media_objects.set_movie(movie)
+            self.clear_widgets()
+            self.accept()
         except ValueError:
             MessageBox(
                 "Missing Values",
                 "You must specify the Name, Runtime, Provider, and Person to create or save a Movie")
 
-    def edit(self, movie: Movie = None, callback: callable = None, index: int = None):
-        """Sets whether the user is editing or adding a Movie in this view
-
-        :param movie: The Movie the user is editing, if any
-        :param callback: The callback function when the user is done editing
-        :param index: The index of the Movie in the media list
-        """
-        self.movie = movie
-        self.callback = callback
-        self.index = index
-
-        self.add_button.setVisible(movie is None)
-        self.save_button.setVisible(movie is not None)
-
-        if movie is not None:
-            self.window().setWindowTitle(f"Edit {movie.get_name()}")
-            self.name_line_edit.setText(movie.get_name())
-            self.runtime_spinner.setValue(movie.get_runtime())
-            self.provider_dropdown.setCurrentText(movie.get_provider().value)
-            self.person_dropdown.setCurrentText(movie.get_person().value)
-            self.start_checkbox.setChecked(movie.is_started())
-            self.finish_checkbox.setChecked(movie.is_finished())
-        else:
-            self.window().setWindowTitle("Add Movie")
-
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
-    widget = MovieView()
+    widget = MovieDialog()
     sys.exit(app.exec_())
