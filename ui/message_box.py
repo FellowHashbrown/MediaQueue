fea2222d92e5b4d6b498c95ce612fd6069cc1910
Jonah Pierce <fellowhashbrown@gmail.com>
2021-03-04 17:33:46 -0500

    Added documentation for MessageBox

--- ui/message_box.py
+++ ui/message_box.py
@@ -1,12 +1,27 @@
+import sys
 from PyQt5 import QtWidgets
 
 
 class MessageBox(QtWidgets.QMessageBox):
+    """The Message Box is an inheritance from QMessageBox
+    for a simplistic Message Box whenever a user forgets to do something
+    and the UI must send an error message
+
+    :param title: The title to set for the MessageBox
+    :param message: The message to display in the MessageBox
+    :param parent: The parent widget for this MessageBox
+    """
     def __init__(self, title: str, message: str, parent: QtWidgets.QWidget = None):
         super().__init__(parent)
-
         self.setIcon(QtWidgets.QMessageBox.Information)
         self.setText(message)
         self.setWindowTitle(title)
         self.setStandardButtons(QtWidgets.QMessageBox.Ok)
-        self.exec()
+        self.exec_()
+
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication([])
+    m = MessageBox("Test Title", "Test Message")
+    m.exec_()
+    sys.exit(app.exec_())
