7e34d1dcbb372be425da890315b881bbdef9014a
Jonah Pierce <fellowhashbrown@gmail.com>
2021-03-04 18:19:36 -0500

    Updated package file for ui

--- ui/__init__.py
+++ ui/__init__.py
@@ -1,12 +1,17 @@
+from ui.media_objects import media_objects
+
 from ui.grid_layout_manager import add_grid_to_layout
 from ui.message_box import MessageBox
 
+from ui.episode import EpisodeDialog
+from ui.episode_list_scroll_area import EpisodeListScrollArea
+from ui.episode_list_widget import EpisodeListWidget
+from ui.media_list_scroll_area import MediaListScrollArea
+from ui.media_list_widget import MediaListWidget
 
-from ui.episode import EpisodeView
-from ui.season import SeasonView
 from ui.limited_series import LimitedSeriesView
 from ui.tv_show import TVShowView
 from ui.podcast import PodcastView
-from ui.movie import MovieView
+from ui.movie import MovieDialog
 from ui.home import Home
 from ui.app import MediaQueue
