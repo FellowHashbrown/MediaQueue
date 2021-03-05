e51322021570dc0f299dc5bdd35eb1b76cacbe80
Jonah Pierce <fellowhashbrown@gmail.com>
2021-03-04 23:44:53 -0500

    Added directory creation for media data

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
-import sys
+import os, sys
 
-from PyQt5.QtWidgets import QApplication
-
+from media import Movie, TVShow, Podcast, LimitedSeries
 from ui import MediaQueue
+
+# Create the directories for the media, if necessary
+if not os.path.exists("data"):
+    os.mkdir("data")
+if not os.path.exists(f"data/{Movie.FOLDER}"):
+    os.mkdir(f"data/{Movie.FOLDER}")
+if not os.path.exists(f"data/{TVShow.FOLDER}"):
+    os.mkdir(f"data/{TVShow.FOLDER}")
+if not os.path.exists(f"data/{Podcast.FOLDER}"):
+    os.mkdir(f"data/{Podcast.FOLDER}")
+if not os.path.exists(f"data/{LimitedSeries.FOLDER}"):
+    os.mkdir(f"data/{LimitedSeries.FOLDER}")
+
 
 if __name__ == "__main__":
     app = MediaQueue(sys.argv)
