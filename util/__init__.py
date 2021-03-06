4917dac0f2e8b14822ac81a33f474ce312f1ed56
Jonah Pierce <fellowhashbrown@gmail.com>
2021-03-06 13:03:14 -0500

    Added options file configuration

--- util/__init__.py
+++ util/__init__.py
@@ -3,3 +3,5 @@ from .export_utils import EXPORTS
 from .import_utils import csv_to_media
 from .export_utils import media_to_csv
 from .export_utils import media_to_json
+
+from .options import options
