3d69eabad0ceb93decaea33b6356fcfa0d593229
Jonah Pierce <fellowhashbrown@gmail.com>
2021-03-06 13:05:47 -0500

    Updating of providers dropdowns across different views happen dynamically

--- ui/app.py
+++ ui/app.py
@@ -25,23 +25,25 @@ class MediaQueue(QtWidgets.QApplication):
         self.widget.currentChanged.connect(self.center)
 
         # Set up the views
-        limited_series_view = LimitedSeriesView(self.widget)
-        podcast_view = PodcastView(self.widget)
-        tv_show_view = TVShowView(self.widget)
-        home_view = Home({
-            "tv_show": tv_show_view,
-            "podcast": podcast_view,
-            "limited_series": limited_series_view
+        self.limited_series_view = LimitedSeriesView(self.widget)
+        self.podcast_view = PodcastView(self.widget)
+        self.tv_show_view = TVShowView(self.widget)
+        self.home_view = Home({
+            "tv_show": self.tv_show_view,
+            "podcast": self.podcast_view,
+            "limited_series": self.limited_series_view
         }, self.widget)
 
         # Setup the Menu Bar for loading data into the Media Queue
         self.window.setMenuBar(AppMenuBar(self.widget,
-                                          update_media_func=home_view.media_list_widget.scroll_area.update_ui))
+                                          update_media_func=self.home_view.media_list_widget.scroll_area.update_ui,
+                                          update_providers_func=self.update_providers,
+                                          update_persons_func=self.update_persons))
 
-        self.widget.addWidget(home_view)
-        self.widget.addWidget(tv_show_view)
-        self.widget.addWidget(podcast_view)
-        self.widget.addWidget(limited_series_view)
+        self.widget.addWidget(self.home_view)
+        self.widget.addWidget(self.tv_show_view)
+        self.widget.addWidget(self.podcast_view)
+        self.widget.addWidget(self.limited_series_view)
 
         self.window.setCentralWidget(self.widget)
         self.window.show()
@@ -56,6 +58,22 @@ class MediaQueue(QtWidgets.QApplication):
         qt_rect = self.widget.currentWidget().frameGeometry()
         qt_rect.moveCenter(MediaQueue.CENTER)
         self.window.move(qt_rect.topLeft())
+
+    # # # # # # # # # # # # # # # # # # # # # # # # #
+
+    def update_providers(self):
+        """Updates any dropdowns that contain the list of providers"""
+        self.home_view.update_providers_filters()
+        self.limited_series_view.update_providers()
+        self.podcast_view.update_providers()
+        self.tv_show_view.update_providers()
+
+    def update_persons(self):
+        """Updates any dropdowns that contain the list of persons"""
+        self.home_view.update_persons_filters()
+        self.limited_series_view.update_persons()
+        self.podcast_view.update_persons()
+        self.tv_show_view.update_persons()
 
 
 if __name__ == "__main__":
